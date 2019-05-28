9f43a61887a202c9199c002d45b4053572db69ef
Matteo Hoffmann <hoffmatteo@gmail.com>
2019-05-28 14:12:04 +0200

    Added register and login

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var pg = require("pg");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 var CON_STRING = process.env.DB_CON_STRING;
 if (CON_STRING == undefined) {
@@ -23,7 +25,7 @@ var app = express();
 
 app.use(session({
     secret: "This is a secret!",
-    resave:true,
+    resave: true,
     saveUninitialized: false
 }));
 
@@ -31,8 +33,75 @@ app.set("views", "views");
 app.set("view engine", "pug");
 
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("login");
 });
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post("/login", urlencodedParser, function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    dbClient.query("SELECT * FROM users WHERE username=$1", [username], function (dbError, dbResponse) {
+        if (dbResponse.rows == 0) {
+            res.render("login", {
+                login_error: "Sorry, username and password do not match!"
+            });
+        } else {
+            bcrypt.compare(password, dbResponse.rows[0].password, function (err, result) { //compares hash with password, result=true if passwords are identical
+
+                if (result) {
+                    req.session.username = username;
+                    req.session.userID = dbResponse.rows[0].id;
+                    console.log(req.session.userID);
+                    res.redirect("search");
+
+                } else {
+
+                    res.render("login", {
+                        login_error: "Sorry, username and password do not match!"
+                    });
+                }
+            })
+
+        }
+    })
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", urlencodedParser, function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    dbClient.query("SELECT * FROM users WHERE username=$1", [username], function (dbError, dbResponse) {
+        if (dbResponse.rows != 0) {
+            res.render("register", {
+                register_error: "Sorry, username already exists."
+            });
+
+        } else {
+
+            bcrypt.hash(password, saltRounds, function (err, hash) {
+
+                dbClient.query("INSERT INTO users (username, password) VALUES ($1, $2)", [username, hash], function (dbError, dbResponse) { //stores username + hash in database
+                    dbClient.query("SELECT * FROM users WHERE username=$1", [username], function (dbError, dbResponse) {
+                        req.session.user = username; //store username and ID in session
+                        req.session.userID = dbResponse.rows[0].id;
+                        console.log(req.session.userID);
+                        res.redirect("search");
+                    });
+
+
+                });
+            });
+        }
+    });
+});
+
 
 app.listen(PORT, function () {
     console.log(`Shopping App listening on Port ${PORT}`);
